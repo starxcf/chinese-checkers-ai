46a43e977a7851b0a63b2bd5de2d57f1e7e2d5e4
Sean <123@321.com>
2019-12-16 22:35:04 -0500

    Update code and documentation

--- OurApproach/cchecker/state.py
+++ OurApproach/cchecker/state.py
@@ -1,4 +1,8 @@
-# -*- coding: utf-8 -*-
+"""
+Chinese checker game status logic. This class handles most of the game logic
+
+@author: Sean, Bill and Alexandra
+"""
 
 __all__ = [
     'GameState',
@@ -42,10 +46,45 @@ class GameState:
             return True
         return False
         
-    def legal_moves(self):pass
-        
+    def legal_moves(self):
+        result = []
+        # map to check already explored moves
+        check_map = np.zeros((BOARD_WIDTH, BOARD_HEIGHT), dtype='uint8')
+        # expand to each directions without jump
+        result.append(checker_pos)
+        check_map[checker_pos] = 1
+        for walk_dir in self.directions:
+            row, col = tuple(map(operator.add, checker_pos, walk_dir))
+            if not board_utils.is_valid_pos(row, col):
+                continue
+            if self.board[row, col, 0] == 0:
+                result.append((row, col))
+                check_map[row, col] = 1
+
+        # check continous jump moves
+        self.board[checker_pos[0], checker_pos[1], 0] = 0;              # Remove current checker before checking
+        self.valid_checker_jump_moves(result, check_map, checker_pos)
+        self.board[checker_pos[0], checker_pos[1], 0] = cur_player;     # Put back current checker
+        result.remove(checker_pos)                                      # Don't allow staying
+        return result
     
-    def is_over(self):pass
+    def is_over(self):
+        cur_board = self.board[:, :, 0]
+        one_win = two_win = True
+        for k in range(BOARD_WIDTH - ROWS_OF_CHECKERS, BOARD_WIDTH):
+            if one_win:
+                up_diag = cur_board.diagonal(k)
+                if not np.array_equal(up_diag, [PLAYER_ONE]*len(up_diag)):
+                    one_win = False
+            if two_win:
+                down_diag = cur_board.diagonal(-k)
+                if not np.array_equal(down_diag, [PLAYER_TWO]*len(down_diag)):
+                    two_win = False
+
+            if not one_win and not two_win:
+                return 0
+
+        return PLAYER_ONE if one_win else PLAYER_TWO
         
     def isonestepmove(self, move):
         ''' Not all 8 around are legal moves, only 6 are. '''
@@ -61,5 +100,50 @@ class GameState:
             return False
         return True
         
-    def isjumpmove(self, move):pass
-        
+    def isjumpmove(self, move):
+                curr_row, curr_col = checker_pos
+        # expand with jump
+        for walk_dir in self.directions:
+            step = 1
+            row_inc, col_inc = walk_dir
+            row, col = curr_row + row_inc, curr_col + col_inc
+            valid_pos = True
+
+            # Go along the direction to find the first checker and record steps
+            while True:
+                if not board_utils.is_valid_pos(row, col):
+                    valid_pos = False
+                    break
+                if self.board[row, col, 0] != 0:
+                    break
+                step += 1
+                row += row_inc
+                col += col_inc
+
+            if not valid_pos:
+                continue
+
+            # Continue in the direction to find the mirror move
+            for i in range(step):
+                row += row_inc
+                col += col_inc
+                if not board_utils.is_valid_pos(row, col) or self.board[row, col, 0] != 0:
+                    valid_pos = False
+                    break
+
+            if not valid_pos:
+                continue
+
+            # get the row and col ready to jump
+            # check whether the destination is visited
+            if check_map[row, col] == 1:
+                continue
+
+            # store moves
+            valid_moves.append((row, col))
+            check_map[row][col] = 1
+            self.valid_checker_jump_moves(valid_moves, check_map, (row, col))
+
+if __name__ == '__main__':
+    b = Board(5)
+    cchecker.printboard(b,'ABCDEFGHIJKLMNOPQRSTUVWXYZ')    
--- OurApproach/playchecker.py
+++ OurApproach/playchecker.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 18 23:08:43 2019
+Pleay Chinese checker game Human vs Computer
 
 @author: Sean, Bill and Alexandra
 """
@@ -9,15 +9,16 @@ import gameplay as g
 import cchecker
 import agent
 from agent.dumb import DumbAgent
+from agent.alphabeta import AlphaBetaAgent
+from agent.MCTS import MCTS
 
 if __name__ == '__main__':
     print(cchecker.welcomemsg())
-#    p1side = cchecker.Player.white
-#    p2side = cchecker.Player.black
-#    player1 = g.GamePlayer(p1side, g.PlayerType.human, agent.Agent())
-#    player2 = g.GamePlayer(p2side, g.PlayerType.computer, DumbAgent())
-#    result = g.play(player1, player2, cchecker.GameState, cchecker.interface)
-#    cchecker.gameend(result)
-#        import cchecker.interface as iface
-    b = cchecker.Board(5)
-    cchecker.printboard(b,'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    p1side = cchecker.Player.white
+    p2side = cchecker.Player.black
+    player1 = g.GamePlayer(p1side, g.PlayerType.human, agent.Agent())
+    player2 = g.GamePlayer(p2side, g.PlayerType.computer, DumbAgent())
+#    player2 = g.GamePlayer(p2side, g.PlayerType.computer, AlphaBetaAgent())
+#    player2 = g.GamePlayer(p2side, g.PlayerType.computer, MCTS())
+    result = g.play(player1, player2, cchecker.GameState, cchecker.interface)
+    cchecker.gameend(result)
